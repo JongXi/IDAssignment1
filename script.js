005417396b040067350ef50fa1b16c485b2a88f3
JongXi <94763363+JongXi@users.noreply.github.com>
2021-12-03 23:41:44 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,72 @@
+// Navigation Bar
+const navMenu = document.querySelector('.navMenu');
+const closeNavButton = document.querySelector('.closeNavButton');
+const navButton = document.querySelector('.navButton');
+
+navButton.addEventListener('click',show);
+closeNavButton.addEventListener('click',close);
+
+function show(){
+  navMenu.style.display = 'flex';
+  navMenu.style.top = '0'
+}
+
+function close(){
+  navMenu.style.top = '-100%';
+}
+
+
+//Education
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+//Contact
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("my-form-status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        status.innerHTML = "<b>Thanks for your submission!</b>";
+        form.reset()
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
